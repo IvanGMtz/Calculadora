3eb3b0a8567d97e870a417b42cc94b323095f094
IvanGMtz <navi.garces24@gmail.com>
2023-05-30 17:13:26 -0500

    style: :lipstick: Add style to the project

---
+++ api.php
@@ -0,0 +1,9 @@
+<?php
+
+    echo <<<HTML
+        Hola Mundo<br><a href="index.html">Volver</a><br>
+    HTML;
+
+    var_dump($_POST);
+
+?>
--- index.php
+++ index.php
@@ -7,6 +7,64 @@
     <title>Formulario</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
+<style>
+*{
+    padding: 0;
+    margin: 0;
+}
+
+body {
+	background: #1c1c1ce5;
+	color: #fff;
+	font-family: 'Roboto', sans-serif;
+}
+
+.container{
+    width: 90%;
+	max-width: 1200px;
+	margin: auto;
+	padding: 40px;
+    gap: 20px;
+    display: grid;
+    grid-template-rows:  0.2fr 1fr;
+}
+
+.form{
+    background-color: #1D202B;
+    border-radius: 20px;
+    padding: 10%;
+    display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+    align-items: center;
+}
+
+.resultado{
+    height: 100px;
+    display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+    align-items: center;
+}
+
+.btn{
+    background: #1f53c5;
+    border: none;
+    padding: 12px;
+    color: white;
+    cursor: pointer;
+    border-radius: 5px;
+}
+
+.datos{
+    color: white;
+    padding: 10px;
+    border-radius: 5px;
+    background: #1D202B;
+    border: 1px solid #1f53c5;
+}
+</style>
 <body>
         <form method="POST" class="form">
             <input type="text" name="N1" placeholder="Ingrese variable 1" class="datos"><br>
